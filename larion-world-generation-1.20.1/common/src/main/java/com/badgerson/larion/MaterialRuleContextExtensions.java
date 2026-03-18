60bf95061e61d845f3fc54fce1f134a8a1a4cef2
Strider3039 <109624374+Strider3039@users.noreply.github.com>
2026-03-18 00:39:59 -0700

    got rid of unnecessary stuff

--- larion-world-generation-1.20.1/common/src/main/java/com/badgerson/larion/MaterialRuleContextExtensions.java
+++
@@ -1,7 +0,0 @@
-package com.badgerson.larion;
-
-import net.minecraft.world.level.levelgen.SurfaceRules;
-
-public interface MaterialRuleContextExtensions {
-  public SurfaceRules.Condition getSomewhatSteepSlopePredicate();
-}
